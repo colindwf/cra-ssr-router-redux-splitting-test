fda91474e6b7b672be9b2bff5a3865ca8f73f216
dengwenfang <dengwenfang@wealink.com>
2018-04-26 14:50:08 +0800

    index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -13,7 +13,7 @@ const actionIndex = (req, res, next) => {
 };
 
 // root (/) should always serve our server rendered page
-router.use('', actionIndex);
+router.use('^/$', actionIndex);
 // other static resources should just be served as they are
 router.use(express.static(
     path.resolve(__dirname, '..', '..', 'build'),
